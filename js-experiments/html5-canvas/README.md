652d1a166823b584b9a82331969e2028f23a7b89
kfrn <knfrances@gmail.com>
2017-02-16 18:46:59 +1300

    Finish exercise

--- js-experiments/html5-canvas/README.md
+++ js-experiments/html5-canvas/README.md
@@ -1,6 +1,6 @@
 Main learning points:
 
 * Understanding how HTML5 canvas works
-
-Notes:
-* You draw on the _context_, not the canvas itself.
+* Drawing in response to mouse movement
+* Changing colour while drawing
+* Quick intro to [global composite operation](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation) (like PS blending modes)
--- js-experiments/html5-canvas/script.js
+++ js-experiments/html5-canvas/script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('#draw')
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d') // You draw on the context, not the canvas itself.
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -7,22 +7,32 @@ canvas.height = window.innerHeight
 ctx.strokeStyle = '#BADA55' // default colour when drawing
 ctx.lineJoin = 'round' // Rounded-edge joins
 ctx.lineCap = 'round' // Stroke rounded off at end
+ctx.lineWidth = 20
 
 let isDrawing = false // flag for mousedown/up
 let lastX = 0
 let lastY = 0
+let hue = 0
 
 function draw(e) {
   if (!isDrawing) return
   console.log(e)
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)` // HSL goes 0-360 but also wraps around
   ctx.beginPath()
   ctx.moveTo(lastX, lastY) // start from here
   ctx.lineTo(e.offsetX, e.offsetY) // go to here
   ctx.stroke()
   // With just the above, super-cool radial effect
+  lastX = e.offsetX
+  lastY = e.offsetY
+  hue++
 }
 
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true)
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true
+  lastX = e.offsetX
+  lastY = e.offsetY
+})
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false) // if you move mouse off canvas
