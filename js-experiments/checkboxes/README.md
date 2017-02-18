247713d69a37a77dc5db3c2884359eb5d89a3aca
kfrn <knfrances@gmail.com>
2017-02-18 13:36:55 +1300

    Add checkboxes exercise

---
+++ js-experiments/checkboxes/README.md
@@ -0,0 +1,5 @@
+Select multiple checkboxes when holding down Shift.
+
+Main learning points:
+* Add event listeners to DOM elements
+* If shift is held, select all checkboxes in range
---
+++ js-experiments/checkboxes/script.js
@@ -0,0 +1,24 @@
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false
+  if (e.shiftKey && this.checked) { // if shift key is down and they are checking the box
+    // Loop over every checkbox and see where checks start and end
+    checkBoxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween // to allow both directions
+        console.log("starting/ending checkbox check");
+      }
+
+      if (inBetween) {
+        checkbox.checked = true
+      }
+    })
+  }
+  lackChecked = this
+}
+
+checkBoxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck)) // Click event will fire even if you use your keyboard
