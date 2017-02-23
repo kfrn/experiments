3c52a80746a3e11f22bdef9d6e836cbdb0ebb680
kfrn <knfrances@gmail.com>
2017-02-24 11:37:48 +1300

    Update script

--- python-csv/README.md
+++ python-csv/README.md
@@ -1,3 +1,5 @@
 ### Intro to Data Manipulation with Python CSV
 
 [NSDR webinar](https://ndsr.americanarchive.org/2017/02/10/aapb-ndsr-webinars-2) presented by Adam Lott, 24 February 2017.
+
+Install [`python-slugify`](https://github.com/un33k/python-slugify) to run script.
--- python-csv/script.py
+++ python-csv/script.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 import csv
+from slugify import slugify
 
-data = []
+def writeFile(source, filename):
+    with open(filename, 'w') as result:
+        writer = csv.writer(result)
+        writer.writerow(header)
+        for row in source:
+            writer.writerow(row)
+
+def parseData(media, is_digitized):
+    print("Working ...")
+    media_filename = slugify(media) # "Film: 16mm" -> "film-16mm" etc
+    digitized = []
+    not_digitized = []
+    for row in reader:
+        if row[3] == media:
+            if is_digitized == True:
+                if row[2]:
+                    print(row)
+                    digitized.append(row)
+                    writeFile(digitized, "digitized_%s.csv" %(media_filename))
+            elif is_digitized == False:
+                if not row[2]:
+                    # print(row)
+                    not_digitized.append(row)
+                    writeFile(not_digitized, "undigitized_%s.csv" %(media_filename))
+    # print("%s items of type '%s' are digitized" %(len(digitized), media))
+    # print("%s items of type '%s' are not digitized" %(len(not_digitized), media))
+    return
 
 with open('csv_data.csv', 'r') as source:
     reader = csv.reader(source)
-    header = next(reader) # Skips first line (headings) and assigns to var
-    for row in reader:
-        if row[3] == "VHS":
-            if not row[2]:
-                data.append(row)
-
-with open("VHS_from_csv_data.csv", 'w') as result:
-    writer = csv.writer(result)
-    writer.writerow(header)
-    for row in data:
-        writer.writerow(row)
+    header = next(reader)
+    # Call function
+    parseData("Film: 16mm", False)
