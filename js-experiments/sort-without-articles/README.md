9fcca96612973aaeddaac0fc1b471516029eb7c8
kfrn <knfrances@gmail.com>
2017-02-26 07:58:25 +1300

    Add sort without articles exercise

---
+++ js-experiments/sort-without-articles/README.md
@@ -0,0 +1,4 @@
+Learning points:
+
+* Sort a list irrespective of articles.
+* Use `removeArticle` in if-statement for sorting, not to actually change the values.
---
+++ js-experiments/sort-without-articles/script.js
@@ -0,0 +1,12 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']
+
+function removeArticle(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim()
+}
+
+const sorted = bands.sort((a,b) => removeArticle(a) > removeArticle(b) ? 1 : -1)
+
+document.querySelector('#bands').innerHTML =
+  sorted
+    .map(band => `<li>${band}</li>`)
+    .join("") // Without join, commas show up between each <li>.
